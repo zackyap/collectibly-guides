86962fff8258ea2f3e97e41a744cf6f22d94bbd1
zavrick <soh.zi.haur@gmail.com>
2014-07-28 14:07:28 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollectiblyGuides::Application.config.secret_key_base = '0121d46a570d6ad0633329753227334ddd9def74002983600f6ca53f1fe836d5baf95a07171b8c7408bc256be4bf0b2d42b86e04bd0d00135b06c78c3d38796a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CollectiblyGuides::Application.config.session_store :cookie_store, key: '_collectibly_guides_session'
