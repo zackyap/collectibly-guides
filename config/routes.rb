f53b6fde8271bf2a3cff22bd2fb22ad9bd24fc1c
zavrick <soh.zi.haur@gmail.com>
2014-08-27 14:55:11 +0800

    Prep meta tags and sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -4,66 +4,12 @@ CollectiblyGuides::Application.routes.draw do
   resources :guides
 
   get "characters/star-lord" => "characters#starlord", as: "gotg_starlord_guide"
-  get "characters/rocket-raccoon" => "characters#rocketraccoon"
-  get "characters/groot" => "characters#groot"
-  get "characters/gamora" => "characters#gamora"
-  get "characters/drax" => "characters#drax"
-  get "characters/ships" => "characters#ships"
+  get "characters/rocket-raccoon" => "characters#rocketraccoon", as: "gotg_rocket_guide"
+  get "characters/groot" => "characters#groot", as: "gotg_groot_guide"
+  get "characters/gamora" => "characters#gamora", as: "gotg_gamora_guide"
+  get "characters/drax" => "characters#drax", as: "gotg_drax_guide"
+  get "characters/ships" => "characters#ships", as: "gotg_ships_guide"
 
   resources :characters
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,33 +13,11 @@ SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.ama
 SitemapGenerator::Sitemap.sitemaps_path = 'collectibly-guides-sitemaps/'
 SitemapGenerator::Sitemap.create do
   add root_path
-  # DbItem.find_each do |db_item|
-  #   add db_item_path(db_item), lastmod: db_item.updated_at, changefreq: 'weekly', priority: 1.0
-  # end
-  # User.find_each do |user|
-  #   add user_path(user), lastmod: user.updated_at, changefreq: 'weekly'
-  # end
-  # Collection.find_each do |collection|
-  #   add user_collection_path(collection.user, collection), lastmod: collection.updated_at, changefreq: 'monthly'
-  # end
-  # Item.find_each do |item|
-  #   add user_item_path(item.user, item), lastmod: item.updated_at, changefreq: 'weekly'
-  # end
-  # Brand.find_each do |brand|
-  #   add brand_path(brand), lastmod: brand.updated_at, changefreq: 'weekly', priority: 0.9
-  # end
-  # Series.find_each do |series|
-  #   add series_path(series), changefreq: 'weekly'
-  # end
-  # ItemSet.find_each do |set|
-  #   add item_set_path(set), changefreq: 'weekly'
-  # end
-  # Review.find_each do |review|
-  #   add db_item_review_path(review.db_item, review), lastmod: review.updated_at, changefreq: 'daily', priority: 1.0
-  # end
-  # Discussion.find_each do |discussion|
-  #   add discussion_path(discussion), lastmod: discussion.updated_at, changefreq: 'daily', priority: 1.0
-  # end
-
+  # add gotg_starlord_guide_path
+  # add gotg_rocket_guide_path
+  # add gotg_groot_guide_path
+  # add gotg_gamora_guide_path
+  # add gotg_drax_guide_path
+  # add gotg_ships_guide_path
 end
 SitemapGenerator::Sitemap.ping_search_engines
