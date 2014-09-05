69a1b490a92aa4058cbc30ce5362bcfdc1f7e26d
zavrick <soh.zi.haur@gmail.com>
2014-09-06 03:17:11 +0800

    Remove unused routes, added links for brands to tags, added favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 CollectiblyGuides::Application.routes.draw do
-  root 'guides#index'
+  # root 'guides#index'
 
-  resources :guides
+  # resources :guides
 
-  get "characters/star-lord" => "characters#starlord", as: "gotg_starlord_guide"
-  get "characters/rocket-raccoon" => "characters#rocketraccoon", as: "gotg_rocket_guide"
-  get "characters/groot" => "characters#groot", as: "gotg_groot_guide"
-  get "characters/gamora" => "characters#gamora", as: "gotg_gamora_guide"
-  get "characters/drax" => "characters#drax", as: "gotg_drax_guide"
-  get "characters/ships" => "characters#ships", as: "gotg_ships_guide"
+  # get "characters/star-lord" => "characters#starlord", as: "gotg_starlord_guide"
+  # get "characters/rocket-raccoon" => "characters#rocketraccoon", as: "gotg_rocket_guide"
+  # get "characters/groot" => "characters#groot", as: "gotg_groot_guide"
+  # get "characters/gamora" => "characters#gamora", as: "gotg_gamora_guide"
+  # get "characters/drax" => "characters#drax", as: "gotg_drax_guide"
+  # get "characters/ships" => "characters#ships", as: "gotg_ships_guide"
   get "stgcc2014" => "characters#stgcc2014", as: "stgcc2014_guide"
 
-  resources :characters
+  # resources :characters
 
 end
